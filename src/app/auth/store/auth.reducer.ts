16be4ee8f8310d49498b820c5c0ff3f066cd8b3d
NehanPathan <pathannehan26@gmail.com>
2024-03-15 12:00:00 +0530

    Add login And logout Actions And Reducer In AuthStore

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,5 +1,6 @@
 import { createReducer, on } from '@ngrx/store';
 import { User } from '../user.model';
+import { login, logout } from './auth.actions';
 
 export interface State {
   user: User;
@@ -12,4 +13,19 @@ const initialState: State = {
   user: null,
 };
 
-export const authReducer = createReducer(initialState);
+export const authReducer = createReducer(
+  initialState,
+  on(login, (state, action) => ({
+    ...state,
+    user: new User(
+      action.email,
+      action.userId,
+      action.token,
+      action.expirationDate
+    ),
+  })),
+  on(logout, (state, action) => ({
+    ...state,
+    user: null,
+  }))
+);
